e0091ea2d1c833888c3709e623b112840e7891cd
jianmos90 <2601237837@qq.com>
2020-06-24 19:47:54 +0800

    2020/6/24 新增：泡沫妖精

--- script/c120120031.lua
+++ script/c120120031.lua
@@ -5,7 +5,6 @@ function cm.initial_effect(c)
 	--Indes Effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(m,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCost(cm.cost)
