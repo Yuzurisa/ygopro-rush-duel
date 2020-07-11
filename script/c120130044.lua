4864805f1a5942e814e81ef09a263630731baf27
jianmos90 <2601237837@qq.com>
2020-07-11 12:00:36 +0800

    2020/7/11 新增：蓝色药剂           修正：怪兽被无效的场合，可以无限发动效果的问题

---
+++ script/c120130044.lua
@@ -0,0 +1,25 @@
+local m=120130044
+local cm=_G["c"..m]
+cm.name="蓝色药剂"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(400)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,400)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
