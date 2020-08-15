3728811b3835d8f63585cc188c56450bb5940ddf
jianmos90 <2601237837@qq.com>
2020-08-15 10:55:24 +0800

    2020/8/15 新增：大贯通！！

---
+++ script/c120140012.lua
@@ -0,0 +1,52 @@
+local m=120140012
+local cm=_G["c"..m]
+cm.name="大贯通！！"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(cm.condition)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.filter(c)
+	return c:IsFaceup() and not c:IsHasEffect(EFFECT_PIERCE)
+end
+function cm.check(g)
+	return g:GetClassCount(Card.GetLevel)==1
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP()
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(cm.filter,tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return g:CheckSubGroup(cm.check,2,2) end
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(cm.filter,tp,LOCATION_MZONE,0,nil)
+	if not g:CheckSubGroup(cm.check,2,2) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(m,1))
+	local sg=g:SelectSubGroup(tp,cm.check,false,2,2)
+	Duel.HintSelection(sg)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(m,2))
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_PIERCE)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
+end
