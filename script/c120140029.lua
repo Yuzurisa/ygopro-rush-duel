c90c2786ebec4af325d9d76efec57c684829c21c
jianmos90 <2601237837@qq.com>
2020-08-16 11:48:23 +0800

    2020/8/16 新增：浆果姐姐（卡名暂译），断绝的回旋壁（卡名暂译）

---
+++ script/c120140029.lua
@@ -0,0 +1,44 @@
+local m=120140029
+local cm=_G["c"..m]
+cm.name="浆果姐姐"
+function cm.initial_effect(c)
+	--Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(cm.condition)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Special Summon
+function cm.confilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_PSYCHO)
+end
+function cm.spfilter(c,e,tp)
+	return c:IsType(TYPE_NORMAL) and c:IsLevel(2) and c:IsRace(RACE_PSYCHO)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(cm.confilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,500) end
+	Duel.PayLPCost(tp,500)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMZoneCount(tp)>0
+		and Duel.IsExistingMatchingCard(cm.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetMZoneCount(tp)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cm.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
