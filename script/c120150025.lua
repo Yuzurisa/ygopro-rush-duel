04eea6272dbfd7ad8ed522fba12b867faccd9e7b
jianmos90 <2601237837@qq.com>
2020-09-14 19:54:33 +0800

    2020/9/14 新增：幻击龙，龙之归还

---
+++ script/c120150025.lua
@@ -0,0 +1,75 @@
+local m=120150025
+local cm=_G["c"..m]
+cm.name="幻击龙 齿车镜龙"
+function cm.initial_effect(c)
+	--Atk Down
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Atk Down
+function cm.costfilter(c)
+	return c:IsLevelAbove(7) and c:IsRace(RACE_DRAGON) and c:IsAbleToGraveAsCost()
+end
+function cm.filter(c)
+	return c:IsFaceup() and c:IsLevelBelow(7)
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,cm.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(m,1))
+	local g=Duel.SelectMatchingCard(tp,cm.filter,tp,0,LOCATION_MZONE,1,2,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		local c=e:GetHandler()
+		local tc=g:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-1500)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			tc=g:GetNext()
+		end
+		if c:IsFaceup() and c:IsRelateToEffect(e) then
+			--Extra Attack
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_BATTLE_DESTROYING)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetCondition(aux.bdcon)
+			e1:SetOperation(cm.chop)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			c:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetDescription(aux.Stringid(m,1))
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_EXTRA_ATTACK)
+			e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e2:SetCondition(cm.atkcon)
+			e2:SetValue(1)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+			c:RegisterEffect(e2)
+		end
+	end
+end
+function cm.chop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(m,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end
+function cm.atkcon(e)
+	return e:GetHandler():GetFlagEffect(m)~=0
+end
