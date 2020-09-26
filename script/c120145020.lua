a1e93b4820c15071fd0c643d860ae9f4695c4980
jianmos90 <2601237837@qq.com>
2020-09-26 10:48:01 +0800

    2020/9/26 新增：宝藏龙

---
+++ script/c120145020.lua
@@ -0,0 +1,36 @@
+local m=120145020
+local cm=_G["c"..m]
+cm.name="宝藏龙"
+function cm.initial_effect(c)
+	--Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(cm.condition)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Draw
+function cm.confilter(c)
+	return c:IsRace(RACE_DRAGON)
+end
+function cm.condition(e)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_HAND+LOCATION_GRAVE)
+		and c:IsReason(REASON_SPSUMMON) and c:IsStatus(STATUS_SPSUMMON_TURN)
+		and Duel.IsExistingMatchingCard(cm.confilter,tp,LOCATION_GRAVE,0,4,nil)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
