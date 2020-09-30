0c67105458ba9d45688e3528add677b2f35eaa29
jianmos90 <2601237837@qq.com>
2020-09-30 19:36:36 +0800

    2020/9/30 新增：生存剑鱼

---
+++ script/c120145039.lua
@@ -0,0 +1,56 @@
+local m=120145039
+local cm=_G["c"..m]
+cm.name="生存剑鱼"
+function cm.initial_effect(c)
+	--Atk Up
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(cm.condition)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Atk Up
+function cm.confilter(c)
+	return c:IsFaceup() and c:IsLevel(7) and c:IsRace(RACE_FISH)
+end
+function cm.costfilter(c)
+	return c:IsRace(RACE_FISH) and c:IsAbleToDeckAsCost()
+end
+function cm.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_FISH)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(cm.confilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,cm.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SendtoDeck(g,nil,1,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(m,1))
+	local g=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_MZONE,0,1,2,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		local tc=g:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(500)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+			tc=g:GetNext()
+		end
+	end
+end
