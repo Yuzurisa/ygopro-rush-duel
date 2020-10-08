94b4d3517b7f24748bc0585f0fd6d32ea9ea6921
jianmos90 <2601237837@qq.com>
2020-10-08 21:03:15 +0800

    2020/10/8 新增：反击盾

--- script/c120145056.lua
+++ script/c120145056.lua
@@ -1,6 +1,6 @@
-local m=120140040
+local m=120145056
 local cm=_G["c"..m]
-cm.name="断绝念力壁"
+cm.name="反击盾"
 function cm.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -14,12 +14,12 @@ function cm.initial_effect(c)
 end
 --Activate
 function cm.costfilter(c)
-	return c:IsRace(RACE_PSYCHO) and c:IsAbleToDeckAsCost()
+	return c:IsType(TYPE_NORMAL) and c:IsDefenseAbove(1000) and c:IsAbleToDeckAsCost()
 end
 function cm.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=Duel.GetAttackTarget()
 	return Duel.GetAttacker():IsControler(1-tp)
-		and c and c:IsControler(tp) and c:IsFaceup() and c:IsRace(RACE_PSYCHO)
+		and c and c:IsControler(tp) and c:IsFaceup() and c:IsType(TYPE_NORMAL)
 end
 function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
@@ -29,12 +29,12 @@ function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoDeck(g,nil,2,REASON_COST)
 end
 function cm.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttackTarget()
-	if tc:IsFaceup() and tc:IsRelateToBattle() then
+	local tc=Duel.GetAttacker()
+	if tc and tc:IsRelateToBattle() and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(500)
+		e1:SetValue(-1000)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
