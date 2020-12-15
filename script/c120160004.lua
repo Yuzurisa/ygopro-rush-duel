81cc97aa6e5807454a080c3881672f9e5b1b2687
jianmos90 <2601237837@qq.com>
2020-12-15 23:21:54 +0800

    2020/12/15 修正bug

--- script/c120160004.lua
+++ script/c120160004.lua
@@ -24,7 +24,7 @@ function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetParam(2)
 	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,2)
 end
-function cm.dactivateisop(e,tp,eg,ep,ev,re,r,rp)
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.DiscardDeck(p,d,REASON_EFFECT)
 	if Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)==3 and Duel.IsPlayerCanDiscardDeck(tp,1)
