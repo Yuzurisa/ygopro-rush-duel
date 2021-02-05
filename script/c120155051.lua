959b329f12a5de65ff346477eecdf62828bcb5ab
jianmos90 <2601237837@qq.com>
2021-02-05 19:19:31 +0800

    2021/2/5 新增：三角重生，更新翻译

---
+++ script/c120155051.lua
@@ -0,0 +1,55 @@
+local m=120155051
+local cm=_G["c"..m]
+cm.name="三角重生"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(cm.condition)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.confilter(c,tp)
+	return c:GetSummonPlayer()==tp
+end
+function cm.tdfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck()
+end
+function cm.tdcheck(g)
+	return g:GetClassCount(Card.GetRace)==1
+end
+function cm.spfilter(c,e,tp)
+	return c:IsLevelBelow(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(cm.confilter,1,nil,1-tp)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(cm.tdfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return g:CheckSubGroup(cm.tdcheck,3,3) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,3,0,0)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(cm.tdfilter,tp,LOCATION_GRAVE,0,nil)
+	if not g:CheckSubGroup(cm.tdcheck,3,3) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=g:SelectSubGroup(tp,cm.tdcheck,false,3,3)
+	if sg:GetCount()>0 then
+		Duel.ConfirmCards(1-tp,sg)
+		if Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)~=0
+			and not Duel.IsExistingMatchingCard(Card.IsAttackPos,tp,LOCATION_MZONE,0,1,nil)
+			and Duel.GetMZoneCount(tp)>0
+			and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(cm.spfilter),tp,LOCATION_GRAVE,0,1,nil,e,tp)
+			and Duel.SelectYesNo(tp,aux.Stringid(m,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local spg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cm.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+			if spg:GetCount()>0 then
+				Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	end
+end
