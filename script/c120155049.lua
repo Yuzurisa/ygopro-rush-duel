e4cb0b9535993bbd61c372da021b40f466a07be8
jianmos90 <2601237837@qq.com>
2021-02-08 19:07:07 +0800

    2021/2/8 新增：鱼族新卡

---
+++ script/c120155049.lua
@@ -0,0 +1,40 @@
+local m=120155049
+local cm=_G["c"..m]
+cm.name="崇高至尊"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.spfilter(c,e,tp)
+	return c:IsRace(RACE_FISH+RACE_SEASERPENT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cm.tdfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck()
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMZoneCount(tp)>0
+		and Duel.IsExistingMatchingCard(cm.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetMZoneCount(tp)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,cm.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)~=0
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(cm.tdfilter),tp,0,LOCATION_GRAVE,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(m,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cm.tdfilter),tp,0,LOCATION_GRAVE,1,5,nil)
+		if sg:GetCount()>0 then
+			Duel.BreakEffect()
+			Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+		end
+	end
+end
