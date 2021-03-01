f2c87baa0200ccbeaab8c5c12dbfc8f211e7836e
jianmos90 <2601237837@qq.com>
2021-03-01 19:06:05 +0800

    2021/3/1 新增：丰沃的蟠龙

---
+++ script/c120170037.lua
@@ -0,0 +1,37 @@
+local m=120170037
+local cm=_G["c"..m]
+cm.name="丰沃的蟠龙"
+function cm.initial_effect(c)
+	--Damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Damage
+function cm.costfilter(c)
+	return c:IsType(TYPE_FIELD) and c:IsAbleToDeckAsCost()
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,cm.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SendtoDeck(g,nil,2,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(600)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,600)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
