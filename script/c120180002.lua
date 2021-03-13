5468aee46ac7318fd27031813d07c333cf70a71d
jianmos90 <2601237837@qq.com>
2021-03-13 20:22:23 +0800

    2021/3/13 新增：监狱之神 迪安·凯特

---
+++ script/c120180002.lua
@@ -0,0 +1,38 @@
+local m=120180002
+local cm=_G["c"..m]
+cm.name="监狱之神 迪安·凯特"
+function cm.initial_effect(c)
+	--Recover
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Recover
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,1-tp,LOCATION_HAND)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if g:GetCount()>0 then
+		Duel.ConfirmCards(tp,g)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local sg=g:Select(tp,1,1,nil)
+		if Duel.SendtoGrave(sg,REASON_EFFECT)~=0 and g:GetFirst():IsLevelAbove(7) then
+			Duel.Recover(tp,1000,REASON_EFFECT)
+		end
+		Duel.ShuffleHand(1-tp)
+	end
+end
