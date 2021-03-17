c942350c3b449aae5a6f9fb352de56f947098227
jianmos90 <2601237837@qq.com>
2021-03-18 07:47:26 +0800

    2021/3/18 更新：CATEGORY_LVCHANGE已被移除

--- script/c120140019.lua
+++ script/c120140019.lua
@@ -5,7 +5,6 @@ function cm.initial_effect(c)
 	--Level Up
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(m,0))
-	e1:SetCategory(CATEGORY_LVCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCost(cm.cost)
