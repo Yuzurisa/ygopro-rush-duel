0460912ce655e694ab5855e8f9545889b8624a64
jianmos90 <2601237837@qq.com>
2021-04-07 20:25:23 +0800

    2021/4/7 新增：蓄积醋酸的落穴

---
+++ script/c120170065.lua
@@ -0,0 +1,27 @@
+local m=120170065
+local cm=_G["c"..m]
+cm.name="蓄积醋酸的落穴"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	if chk==0 then return ep~=tp and tc:IsLocation(LOCATION_MZONE)
+		and tc:IsFaceup() and tc:IsType(TYPE_EFFECT) and tc:IsLevelBelow(3) end
+	Duel.SetTargetCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
