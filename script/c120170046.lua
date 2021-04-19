433e4ebaeecbae05230b49b5a67f701a1e6e286e
jianmos90 <2601237837@qq.com>
2021-04-19 21:05:37 +0800

    2021/4/19 新增：美☆魔女狩猎，更新翻译

---
+++ script/c120170046.lua
@@ -0,0 +1,53 @@
+local m=120170046
+local cm=_G["c"..m]
+cm.name="美☆魔女狩猎"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(cm.condition)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.confilter(c)
+	return c:IsFaceup() and c:IsLevelAbove(8) and c:IsRace(RACE_AQUA)
+end
+function cm.costfilter(c)
+	return c:IsRace(RACE_AQUA) and c:IsAbleToGraveAsCost()
+end
+function cm.desfilter(c)
+	return c:IsFaceup() and c:IsLevelBelow(8)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(cm.confilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,cm.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.desfilter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(cm.desfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,cm.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		if Duel.Destroy(g,REASON_EFFECT)~=0
+			and bit.band(g:GetFirst():GetPreviousRaceOnField(),RACE_SPELLCASTER)~=0
+			and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+			and Duel.SelectYesNo(tp,aux.Stringid(m,1)) then
+			local dg=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+			Duel.Destroy(dg,REASON_EFFECT)
+		end
+	end
+end
