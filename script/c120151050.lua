86fc2588cecd9fbb98791cb65c4fc573c10b84f1
jianmos90 <2601237837@qq.com>
2021-05-26 22:06:15 +0800

    2021/5/26 新增：后仰射击

---
+++ script/c120151050.lua
@@ -0,0 +1,33 @@
+local m=120151050
+local cm=_G["c"..m]
+cm.name="后仰射击"
+function cm.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetCode(EVENT_DRAW)
+	e1:SetCondition(cm.condition)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+function cm.filter(c)
+	return c:IsFaceup() and c:IsLevelBelow(8) and c:IsAbleToHand()
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and bit.band(r,REASON_RULE)~=0
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.filter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(cm.filter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,cm.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
