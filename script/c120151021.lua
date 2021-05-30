032ebb5cd801d24e221c73bf186708afe191828c
jianmos90 <2601237837@qq.com>
2021-05-30 08:47:05 +0800

    2021/5/30 新增：电子界新卡

---
+++ script/c120151021.lua
@@ -0,0 +1,42 @@
+local m=120151021
+local cm=_G["c"..m]
+cm.name="黑种子粒子机·布拉瑟伦"
+function cm.initial_effect(c)
+	--Discard Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_TOHAND+CATEGORY_GRAVE_ACTION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(cm.condition)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Discard Deck
+function cm.thfilter(c)
+	return c:IsRace(RACE_CYBERSE) and c:IsDefense(0) and c:IsAbleToHand()
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_SUMMON) and c:IsStatus(STATUS_SUMMON_TURN)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,1)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.DiscardDeck(tp,1,REASON_EFFECT)==0 then return end
+	local g=Duel.GetOperatedGroup()
+	local ct=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if g:IsExists(Card.IsRace,1,nil,RACE_CYBERSE) and ct>0
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(cm.thfilter),tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(m,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cm.thfilter),tp,LOCATION_GRAVE,0,1,ct,nil)
+		if sg:GetCount()>0 then
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+		end
+	end
+end
