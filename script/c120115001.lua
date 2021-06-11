32bdf2669e2f82f911b4bb0fd512cbbfb0cd4013
李启湛 <2601237837@qq.com>
2021-06-11 21:02:30 +0800

    2023/6/11 修正：七星道魔女的bug

--- script/c120115001.lua
+++ script/c120115001.lua
@@ -44,12 +44,11 @@ function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 		and g:GetFirst():IsCode(list[1])
 		and c:IsFaceup()
 		and c:IsRelateToEffect(e) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(400)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e1)
-		end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(400)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
 	end
 end
