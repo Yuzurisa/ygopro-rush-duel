c11a00421269d917957a390bfc52e5185d73865e
jianmos90 <2601237837@qq.com>
2021-08-03 21:09:35 +0800

    2021/8/3 新增：加速魔杖手·烈焰魔女，更新翻译

---
+++ script/c120186010.lua
@@ -0,0 +1,55 @@
+local m=120186010
+local cm=_G["c"..m]
+cm.name="加速魔杖手·烈焰魔女"
+function cm.initial_effect(c)
+	--Damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(cm.cost)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Damage
+function cm.costfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckAsCost()
+end
+function cm.filter(c)
+	return c:IsFaceup() and not c:IsRace(RACE_SPELLCASTER)
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,cm.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SendtoDeck(g,nil,2,REASON_COST)
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(400)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,400)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Damage(p,d,REASON_EFFECT)~=0 and p==1-tp
+		and Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(m,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(m,2))
+		local g=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_MZONE,0,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.HintSelection(g)
+			local tc=g:GetFirst()
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CHANGE_RACE)
+			e1:SetValue(RACE_SPELLCASTER)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
