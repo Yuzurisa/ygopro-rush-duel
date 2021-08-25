5c43df3579944767d6f26162cc5c16c07f082580
jianmos90 <2601237837@qq.com>
2021-08-25 21:20:01 +0800

    2021/8/25 新增：极超成金恐龙王

---
+++ script/c120196037.lua
@@ -0,0 +1,57 @@
+local m=120196037
+local list={120130029,120151010,120151018}
+local cm=_G["c"..m]
+cm.name="极超成金恐龙王"
+function cm.initial_effect(c)
+	aux.AddCodeList(c,list[1],list[2],list[3])
+	--Fusion Material
+	aux.AddFusionProcCode2(c,list[1],list[2],true,true)
+	--Atk Up
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_TOHAND+CATEGORY_GRAVE_ACTION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(cm.cost)
+	e1:SetOperation(cm.operation)
+	c:RegisterEffect(e1)
+end
+--Atk Up
+function cm.costfilter(c)
+	return c:IsRace(RACE_DINOSAUR) and c:IsAbleToDeckAsCost()
+end
+function cm.exfilter(c)
+	return RushDuel.IsLegendCode(c,list[3])
+end
+function cm.thfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+end
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,cm.costfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	local op=Duel.SelectOption(p,aux.Stringid(m,1),aux.Stringid(m,2))
+	Duel.SendtoDeck(g,nil,op,REASON_COST)
+end
+function cm.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(700)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		if Duel.IsExistingMatchingCard(cm.exfilter,tp,LOCATION_GRAVE,0,1,nil)
+			and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(cm.thfilter),tp,LOCATION_GRAVE,0,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(m,3)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cm.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+			if sg:GetCount()>0 then
+				Duel.SendtoHand(sg,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,sg)
+			end
+		end
+	end
+end
